30c52ffdba668827837d0e0a225b1be36dbd6ddd
刘启明 <455043818@qq.com>
2019-10-08 11:47:05 +0800

    IText7.0生成PDF文件（中文汉字）

--- aehyok.Core.Web/Controllers/HomeController.cs
+++ aehyok.Core.Web/Controllers/HomeController.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using aehyok.Core.Web.Models;
+using iText.Kernel.Font;
+using iText.IO.Font;
+using iText.Kernel.Pdf;
+using iText.Layout.Element;
+using iText.Layout;
 
 namespace aehyok.Core.Web.Controllers
 {
@@ -20,6 +25,12 @@ namespace aehyok.Core.Web.Controllers
 
         public IActionResult Index()
         {
+            PdfFont font = PdfFontFactory.CreateFont(@"AdobeSongStd-Light.otf", PdfEncodings.IDENTITY_H, false);
+            PdfWriter pdfWriter = new PdfWriter("hello.pdf");
+            PdfDocument pdfDocument = new PdfDocument(pdfWriter);
+            Document document = new Document(pdfDocument);
+            document.Add(new Paragraph("你好!").SetFont(font));
+            document.Close();
             return View();
         }
 
