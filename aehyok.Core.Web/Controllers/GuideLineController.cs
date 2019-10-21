fc7c755082c72cabfb0f09a1a5fd0bef537ae6bc
刘启明 <455043818@qq.com>
2019-10-21 10:19:13 +0800

    访问swagger首页

---
+++ aehyok.Core.Web/Controllers/GuideLineController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aehyok.Core.Web.Controllers
+{
+
+    /// <summary>
+    /// 指标定义器
+    /// </summary>
+    public class GuideLineController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- aehyok.Core.Web/Startup.cs
+++ aehyok.Core.Web/Startup.cs
@@ -155,7 +155,7 @@ namespace aehyok.Core.Web
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "aehyok Web Api V1");
-                c.RoutePrefix = string.Empty;
+                //c.RoutePrefix = string.Empty;
             });
 
             app.UseHttpsRedirection();
