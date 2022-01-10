7f25eb7d17b63f98037c0139415dbe611e6852c7
刘启明 <455043818@qq.com>
2022-01-10 21:42:09 +0800

    feat(react): 结果列表

---
+++ react-antd-pro/src/pages/system/guideline-manage/result-table/index.tsx
@@ -0,0 +1,178 @@
+import React, { useRef, useState } from 'react';
+import type { ProColumns } from '@ant-design/pro-table';
+import { EditableProTable } from '@ant-design/pro-table';
+import type { ActionType } from '@ant-design/pro-table';
+import { Button, Input, Space, Tag, Form } from 'antd';
+import styles from '../index.less'
+import { CheckCircleOutlined, CopyOutlined, DeleteOutlined, PlusOutlined, ScissorOutlined } from '@ant-design/icons';
+
+const waitTime = (time: number = 100, key: any, rows: any) => {
+  console.log(key, rows, 'values', )
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, time);
+  });
+};
+
+
+type DataSourceType = {
+  id: React.Key;
+  parameterName?: string;
+  displayTitle?: string;
+  parameterType?: string;
+  displayOrder?: number;
+};
+
+const defaultData: DataSourceType[] = [
+  {
+      "id": 1,
+      "parameterName": "p_nf",
+      "displayTitle": "统计年份",
+      "displayOrder": 1,
+      "parameterType": "数值型"
+  },
+  {
+      "id": 2,
+      "parameterName": "p_hy",
+      "displayTitle": "行业类型",
+      "displayOrder": 2,
+      "parameterType": "代码表"
+  }
+]
+
+const columns: ProColumns<DataSourceType>[] = [
+  {
+    key: '1',
+    title: '参数名称',
+    dataIndex: 'parameterName',
+    formItemProps: {
+      rules: [
+        {
+          required: true,
+          message: '请输入参数名称',
+        },
+      ],
+    },
+    width: '22%',
+  },
+  {
+    key: '2',
+    title: '显示名称',
+    dataIndex: 'displayTitle',
+    width: '20%',
+  },
+  {
+    key: '3',
+    title: '参数类型',
+    dataIndex: 'parameterType',
+    valueType: 'select',
+    width: '20%',
+    valueEnum: {
+      all: { text: '代码表', status: 'Default' },
+      open: {
+        text: '字符型',
+        status: 'Error',
+      },
+      closed: {
+        text: '已解决',
+        status: 'Success',
+      },
+    },
+  },
+  {
+    key: '4',
+    title: '顺序',
+    dataIndex: 'displayOrder',
+    width: '20%',
+  },
+  {
+    title: '操作',
+    valueType: 'option',
+    width: 120,
+    render: (text, record, _, action) => [
+      <a
+        key="editable"
+        onClick={() => {
+          action?.startEditable?.(record.id);
+        }}
+      >
+        编辑
+      </a>,
+      <EditableProTable.RecordCreator
+        key="copy"
+        record={{
+          ...record,
+          id: (Math.random() * 1000000).toFixed(0),
+        }}
+      >
+        <a>删除</a>
+      </EditableProTable.RecordCreator>,
+    ],
+  },
+];
+
+const ParameterTable = () => {
+  const actionRef = useRef<ActionType>();
+  const [editableKeys, setEditableRowKeys] = useState<React.Key[]>([]);
+  const [dataSource, setDataSource] = useState<DataSourceType[]>([]);
+  const [form] = Form.useForm();
+  return (
+    <>
+      <Space>
+        <Button
+          type="primary"
+          onClick={() => {
+            actionRef.current?.addEditRecord?.({
+              id: (Math.random() * 1000000).toFixed(0),
+              title: '添加参数',
+            });
+          }}
+          icon={<PlusOutlined />}
+        >
+          添加参数
+        </Button>
+        <Button
+          key="rest"
+          onClick={() => {
+            form.resetFields();
+          }}
+        >
+          重置表单
+        </Button>
+        {/* <Button type="dashed" icon={<DeleteOutlined />} className={styles.buttonmarginright}>删除参数</Button> */}
+        <Button type="dashed" icon={<CopyOutlined />} className={styles.buttonmarginright}>复制参数</Button>
+        <Button type='dashed' icon={<ScissorOutlined />} className={styles.buttonmarginright}>粘贴参数</Button>
+        {/* <Button type="dashed" icon={<CheckCircleOutlined />} className={styles.buttonmarginright}>保存</Button> */}
+        {/* <Button type='dashed' icon={<DeleteOutlined />}>取消</Button> */}
+      </Space>
+
+      <EditableProTable<DataSourceType>
+        rowKey="id"
+        actionRef={actionRef}
+        maxLength={5}
+        // 关闭默认的新建按钮
+        recordCreatorProps={false}
+        columns={columns}
+        request={async () => ({
+          data: defaultData,
+          total: 3,
+          success: true,
+        })}
+        value={dataSource}
+        onChange={setDataSource}
+        editable={{
+          form,
+          editableKeys,
+          onSave: async (key,rows) => {
+            await waitTime(2000,key, rows);
+          },
+          onChange: setEditableRowKeys,
+          actionRender: (row, config, dom) => [dom.save, dom.cancel],
+        }}
+      />
+    </>
+  );
+};
+
+export default ParameterTable;
--- react-antd-pro/src/pages/system/guideline-manage/tab/index.tsx
+++ react-antd-pro/src/pages/system/guideline-manage/tab/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { Card, Tabs } from 'antd';
 import ParameterTable from '../parameter-table';
+import ResultTable from '../result-table';
 const { TabPane } = Tabs;
 const GuidelineTable = (props: any) => {
   const { guidelineData } = props
@@ -19,7 +20,7 @@ const GuidelineTable = (props: any) => {
           <ParameterTable />
         </TabPane>
         <TabPane tab="显示结果字段定义" key="2">
-          Content of Tab Pane 2
+          <ResultTable />
         </TabPane>
       </Tabs>
     </Card>
