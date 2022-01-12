50860b7a97dbed1ab0209c5c3dca296d11db40f5
刘启明 <455043818@qq.com>
2022-01-13 07:31:37 +0800

    fix(react): update 菜单布局

--- react-antd-pro/src/pages/system/menu-manage/modal/index.tsx
+++ react-antd-pro/src/pages/system/menu-manage/modal/index.tsx
@@ -1,8 +1,14 @@
 import { Modal, Form,Input, Button, message, Select, Skeleton } from 'antd';
 import { useRef, useEffect, useState } from 'react';
 const { Option } = Select;
+import ProForm, {
+  ProFormText,
+  ProFormDateRangePicker,
+  ProFormTextArea,
+  ProFormSelect
+} from '@ant-design/pro-form';
 import { getUser } from '@/services/ant-design-pro/api'
-// eslint-disable-next-line @typescript-eslint/ban-types
+
 export default (props: any) => {
   const { modalVisible, hiddenModal, editId, actionRef } = props
   console.log(props.modalVisible, editId, 'ssss----ss')
@@ -27,10 +33,6 @@ export default (props: any) => {
     console.log('---fetch---')
   }, [])
 
-  const layout = {
-    labelCol: { span: 6 },
-    wrapperCol: { span: 18 },
-  };
   const formRef = useRef(null);
   const [form] = Form.useForm();
 
@@ -44,69 +46,58 @@ export default (props: any) => {
     message.success('结果保存成功')
   }
 
-  const onChange = () => {}
-
-  const onSearch = () => {}
   return (
-    <Modal title="添加用户" footer={null} visible={modalVisible} onOk={handleOk} onCancel={handleCancel}>
+    <Modal title="添加菜单" footer={null} visible={modalVisible} onOk={handleOk} onCancel={handleCancel}>
       {
         initialValues === undefined && editId !== undefined ? <Skeleton /> :
-
-      <Form form={form} onFinish={(values: any)=> onSubmit(values) } ref={formRef} {...layout}  initialValues={initialValues}>
-        <Form.Item
-          label="姓名"
-          name="nickName"
-          rules={[{ required: true, message: '请输入姓名' }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="账号"
-          name="account"
-          // rules={[{ required: true, message: '请输入账号' }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="性别"
-          name="sex"
-        >
-          <Select
-            showSearch
-            placeholder="请选择性别"
-            optionFilterProp="children"
-            onChange={onChange}
-            onSearch={onSearch}
-          >
-            <Option value="0">未知</Option>
-            <Option value="1">男</Option>
-            <Option value="2">女</Option>
-          </Select>
-        </Form.Item>
-        <Form.Item
-          label="用户角色"
-          name="roleInfo"
-        >
-        <Select
-            showSearch
-            placeholder="请选择用户角色"
-            optionFilterProp="children"
-            onChange={onChange}
-            onSearch={onSearch}
-          >
-            <Option value="1">管理员</Option>
-            <Option value="2">普通用户</Option>
-          </Select>
-        </Form.Item>
-        <Form.Item style={{textAlign:'right'}}>
-          <Button type="primary" htmlType="submit" style={{ marginRight :'20px'}} >
-            保存
-          </Button>
+        <ProForm
+          submitter={false}
+          onFinish={async (values) => onSubmit(values)}
+      >
+        <ProForm.Group>
+          <ProFormText
+            name="name"
+            label="菜单名称"
+            placeholder="请输入菜单名称"
+          />
+          <ProFormText
+            name="path"
+            label="菜单路由"
+          />
+        </ProForm.Group>
+        <ProForm.Group>
+          <ProFormText
+            name="displayOrder"
+            label="显示顺序"
+            placeholder="请输入显示顺序"
+          />
+        <ProFormSelect
+          options={[
+            {
+              value: '6',
+              label: '禁用',
+            },
+            {
+              value: '12',
+              label: '启用',
+            },
+          ]}
+          initialValue="6"
+          width="xs"
+          name="taxRate"
+          label="是否禁用"
+        />
+        </ProForm.Group>
+        <ProFormTextArea width="xl" label="参数" name="remark"  autoSize ={{minRows: 6, maxRow: 10}} />
+        <ProForm.Group style={{textAlign:'right'}}>
           <Button htmlType="button" onClick={ handleCancel }>
             取消
           </Button>
-        </Form.Item>
-      </Form>
+          <Button type="primary" htmlType="submit" style={{ marginRight :'20px'}} >
+            保存
+          </Button>
+        </ProForm.Group>
+      </ProForm>
     }
     </Modal>
   );
