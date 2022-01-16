832cb5d6e0ee3450be48bde8ede81520858bdf16
aehyok <455043818@qq.com>
2022-01-16 21:53:19 +0800

    feat(react): logicflow 引入，待继续使用API

--- react-antd-pro/src/pages/system/flow-manage/index.tsx
+++ react-antd-pro/src/pages/system/flow-manage/index.tsx
@@ -1,14 +1,28 @@
 import { PageContainer } from '@ant-design/pro-layout'
-import React from 'react'
+import LogicFlow from "@logicflow/core";
+import "@logicflow/core/dist/style/index.css";
+import { useEffect, useRef } from "react";
 
-const index = () => {
+const FlowManage = () => {
+
+  const refContainer = useRef();
+  useEffect(() => {
+    const logicflow = new LogicFlow({
+      container: refContainer.current,
+      grid: true,
+      width: 1000,
+      height: 500
+    });
+    logicflow.render();
+  }, []);
+
   return (
     <div>
       <PageContainer>
-        流程配置器
+      <div  ref={refContainer} />;
       </PageContainer>
     </div>
   )
 }
 
-export default index
+export default FlowManage
