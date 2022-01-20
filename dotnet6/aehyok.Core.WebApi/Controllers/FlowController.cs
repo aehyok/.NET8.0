7021374951d6c91206d38cebdb24677c7655cd40
aehyok <455043818@qq.com>
2022-01-21 06:53:35 +0800

    Update FlowController.cs

--- dotnet6/aehyok.Core.WebApi/Controllers/FlowController.cs
+++ dotnet6/aehyok.Core.WebApi/Controllers/FlowController.cs
@@ -29,5 +29,62 @@ namespace aehyok.Core.WebApi.Controllers
         {
             return await this._flowRepository.GetFlowEntityTypeList();
         }
+
+        /// <summary>
+        /// 保存流程
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<int> SaveFlowEntityType (FlowEntityType flowEntityType)
+        {
+            return await this._flowRepository.SaveFlowEntityType(flowEntityType);
+        }
+
+        /// <summary>
+        /// 获取流程详情
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<FlowEntityType> GetFlowEntityType(string flowId)
+        {
+            return await this._flowRepository.GetFlowEntityTypeById(flowId);
+        }
+
+        /// <summary>
+        /// 删除流程
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<int> DeleteFlowEntityType(string flowId)
+        {
+            return await this._flowRepository.DeleteFlowEntityType(flowId);
+        }
+
+        /// <summary>
+        /// 通过流程ID获取流程状态列表
+        /// </summary>
+        /// <param name="flowId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<List<FlowEntityState>> GetFlowEntityStateList(string flowId)
+        {
+            return await this._flowRepository.GetFlowEntityStateList(flowId);
+        }
+
+        /// <summary>
+        /// 通过状态Id获取流程动作列表
+        /// </summary>
+        /// <param name="stateId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<List<FlowStateTransition >> GetFlowEntityActionList(string stateId)
+        {
+            return await this._flowRepository.GetFlowEntityTransitions(stateId);
+        }
     }
 }
