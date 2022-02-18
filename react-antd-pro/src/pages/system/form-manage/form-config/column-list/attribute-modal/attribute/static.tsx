fae4493ff4052b69593dde86e192fc5554f3f829
刘启明 <455043818@qq.com>
2022-02-18 11:30:19 +0800

    feat(react): 表单管理 字段类型枚举的优化处理

---
+++ react-antd-pro/src/pages/system/form-manage/form-config/column-list/attribute-modal/attribute/static.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AttributeStatic = () => {
+  return (
+    <div>
+      AttributeStatic
+    </div>
+  )
+}
+
+export default AttributeStatic
--- react-antd-pro/src/pages/system/form-manage/form-config/column-list/attribute-modal/index.tsx
+++ react-antd-pro/src/pages/system/form-manage/form-config/column-list/attribute-modal/index.tsx
@@ -1,82 +1,93 @@
-import { Modal, Form,Input, Button, message } from 'antd';
+import { Modal, Form, Input, Button, message } from 'antd';
 import { useRef } from 'react';
 import { insertSystemForm } from '@/services/ant-design-pro/form'
-import  { ColumnType } from '@/services/ant-design-pro/typings.d.ts'
+import { ColumnType } from '@/services/ant-design-pro/typings.d.ts'
 import AttributeText from './attribute/text'
+import AttributeTextarea from './attribute/textarea'
 import AttributeNumber from './attribute/number'
 import AttributeDate from './attribute/date'
 import AttributeDaterange from './attribute/daterange'
+import AttributeEditor from './attribute/editor'
+import AttributeImage from './attribute/image'
+import AttributeSelect from './attribute/select'
+import AttributeStatic from './attribute/static'
 // eslint-disable-next-line @typescript-eslint/ban-types
+import AttributeVideo from './attribute/video'
+const AttributeModel = (props: { modalVisible: boolean, hiddenModal: Function, refresh: Function, currentRecord: any }) => {
+    const { modalVisible, hiddenModal, refresh, currentRecord } = props
+    console.log(props.modalVisible, modalVisible, 'ssss----ss')
 
-const AttributeModel = (props: {modalVisible: boolean, hiddenModal: Function, refresh: Function, currentRecord: any}) => {
-  const { modalVisible, hiddenModal, refresh, currentRecord } = props
-  console.log(props.modalVisible, modalVisible, 'ssss----ss')
+    console.log(currentRecord, 'currentRecord')
 
-  console.log(currentRecord, 'currentRecord')
 
-  const handleOk = () => {
-    hiddenModal()
-  }
-  const columnType = ColumnType[currentRecord.type]
-  const ReturnComponent = () => {
-      console.log(currentRecord.type, 'return component')
-     switch(currentRecord.type) {
-         case 'text': 
-            return <AttributeText />;
-         case 'number': 
-            return <AttributeNumber />;
-         case 'date': 
-            return <AttributeDate />;
-         case 'daterange': 
-            return <AttributeDaterange />;
-         default:
-             break;   
-     }
-}
-
-  const formRef = useRef(null);
-  const [form] = Form.useForm();
-
-  const handleCancel = () => {
-    form.resetFields()
-    hiddenModal()
-  }
-
-  const onSubmit = async(values: any) => {
-    const result = await insertSystemForm({
-      formName: values.formName,
-    })
-    if(result.code === 200) {
-      message.success('新增表单成功')
-      handleCancel()
-      refresh()
-      // TODO 还没有刷新左侧树
+    const handleOk = () => {
+        hiddenModal()
     }
-    console.log(result, '保存结果')
-  }
- 
-  return (
-    <Modal title={currentRecord.title+ '-' + columnType} footer={null} visible={modalVisible} onOk={handleOk} onCancel={handleCancel}>
-      <Form form={form} onFinish={(values: any)=> onSubmit(values) } ref={formRef}>
-        {/* <Form.Item
+    const columnType = ColumnType[currentRecord.type]
+
+    const getAttributeComponent = (
+        // type: ColumnType,
+        // props: { attribute: any; cId: string },
+    ) => {
+        const type = currentRecord.type
+        const attributeComponent: { [T in ColumnType]: React.ReactElement } = {
+            date: <AttributeDate />,
+            daterange: <AttributeDaterange />,
+            editor: <AttributeEditor />,
+            image: <AttributeImage />,
+            number: <AttributeNumber />,
+            select: <AttributeSelect />,
+            static: <AttributeStatic />,
+            text: <AttributeText />,
+            textarea: <AttributeTextarea />,
+            video: <AttributeVideo />
+        };
+        return attributeComponent[type];
+    };
+
+    const formRef = useRef(null);
+    const [form] = Form.useForm();
+
+    const handleCancel = () => {
+        form.resetFields()
+        hiddenModal()
+    }
+
+    const onSubmit = async (values: any) => {
+        const result = await insertSystemForm({
+            formName: values.formName,
+        })
+        if (result.code === 200) {
+            message.success('新增表单成功')
+            handleCancel()
+            refresh()
+            // TODO 还没有刷新左侧树
+        }
+        console.log(result, '保存结果')
+    }
+
+    return (
+        <Modal title={currentRecord.title + '-' + columnType} footer={null} visible={modalVisible} onOk={handleOk} onCancel={handleCancel}>
+            <Form form={form} onFinish={(values: any) => onSubmit(values)} ref={formRef}>
+                {/* <Form.Item
           label="表单名称"
           name="formName"
           rules={[{ required: true, message: '请输入表单名称' }]}
         >
           <Input />
         </Form.Item> */}
-        {ReturnComponent()}
-        <Form.Item style={{textAlign:'right'}}>
-          <Button type="primary" htmlType="submit" style={{ marginRight :'20px'}} >
-            保存
-          </Button>
-          <Button htmlType="button" onClick={ handleCancel }>
-            取消
-          </Button>
-        </Form.Item>
-      </Form>
-    </Modal>
-  );
+                {getAttributeComponent()}
+                <Form.Item style={{ textAlign: 'right' }}>
+                    <Button type="primary" htmlType="submit" style={{ marginRight: '20px' }} >
+                        保存
+                    </Button>
+                    <Button htmlType="button" onClick={handleCancel}>
+                        取消
+                    </Button>
+                </Form.Item>
+            </Form>
+        </Modal>
+    );
 };
 
 export default AttributeModel
