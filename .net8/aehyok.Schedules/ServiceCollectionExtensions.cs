d22b3227fd007b00a8224a1f84252cc86ff58e6c
刘启明 <455043818@qq.com>
2023-12-15 18:29:41 +0800

    Update ServiceCollectionExtensions.cs

--- .net8/aehyok.Schedules/ServiceCollectionExtensions.cs
+++ .net8/aehyok.Schedules/ServiceCollectionExtensions.cs
@@ -17,21 +17,7 @@ namespace aehyok.Schedules
         /// <returns></returns>
         public static IServiceCollection AddCronServices(this IServiceCollection services)
         {
-            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-            
             var cronType = typeof(CronScheduleService);
-
-            //foreach (Assembly assembly in assemblies)
-            //{
-            //    foreach(var type in assembly.GetTypes())
-            //    {
-            //        //判断type是否继承了CronScheduleService类
-            //        if(type.IsAssignableTo(cronType) && !type.IsAbstract)
-            //        {
-            //            services.Add(new ServiceDescriptor(typeof(IHostedService), type, ServiceLifetime.Singleton));
-            //        }
-            //    }
-            //}
 
             foreach (var type in TypeFinders.SearchTypes(cronType, TypeFinders.TypeClassification.Class))
             {
