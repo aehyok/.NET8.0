145ef6d17a5a3e7db4ef5765626a624fdb593569
刘启明 <455043818@qq.com>
2023-12-15 19:16:54 +0800

    chore: remove

--- .net8/aehyok.EntityFramework/IScopedDependency.cs
+++ .net8/aehyok.EntityFramework/IScopedDependency.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace aehyok.EntityFramework
 {
     /// <summary>
-    /// 所有的EFCore 的Repository Service 都需要继承此接口
+    /// 所有的EFCore 的Repository Service 都需要继承此接口,便于通过反射进行注入
     /// </summary>
     public interface IScopedDependency
     {
