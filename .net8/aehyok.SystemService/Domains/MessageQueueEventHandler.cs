f6231ba1ddf1e123d84569c79259250677b69220
刘启明 <455043818@qq.com>
2023-12-19 12:38:12 +0800

    feat: add System table

---
+++ .net8/aehyok.SystemService/Domains/MessageQueueEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using aehyok.EntityFramework.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aehyok.SystemService.Domains
+{
+    /// <summary>
+    /// RabbitMQ消息队列事件处理器
+    /// </summary>
+    public class MessageQueueEventHandler: EntityBase
+    {
+
+    }
+}
---
+++ .net8/aehyok.SystemService/Migrations/20231219043709_AddFile.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aehyok.SystemService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "File",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    IsDeleted = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    CreatedBy = table.Column<long>(type: "bigint", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UpdatedBy = table.Column<long>(type: "bigint", nullable: true),
+                    Remark = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_File", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_File_User_CreatedBy",
+                        column: x => x.CreatedBy,
+                        principalTable: "User",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_File_User_UpdatedBy",
+                        column: x => x.UpdatedBy,
+                        principalTable: "User",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_File_CreatedBy",
+                table: "File",
+                column: "CreatedBy");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_File_UpdatedBy",
+                table: "File",
+                column: "UpdatedBy");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "File");
+        }
+    }
+}
