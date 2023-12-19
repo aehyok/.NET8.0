15b3ac313ef8f5b997d5dc72f7797c966c8a9b2a
刘启明 <455043818@qq.com>
2023-12-19 10:02:32 +0800

    feat:控制器添加

---
+++ .net8/aehyok.NCDP.Api/Controllers/NCDPControllerBase.cs
@@ -0,0 +1,12 @@
+﻿using aehyok.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aehyok.NCDP.Api.Controllers
+{
+    [Route("api/NCDP/[controller]")]
+    public class NCDPControllerBase : ApiControllerBase
+    {
+
+    }
+}
---
+++ .net8/aehyok.NCDP.Api/Controllers/RoleController.cs
@@ -0,0 +1,22 @@
+﻿using aehyok.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aehyok.NCDP.Api.Controllers
+{
+    /// <summary>
+    /// 角色管理
+    /// </summary>
+    public class RoleController : NCDPControllerBase
+    {
+        /// <summary>
+        /// 测试接口
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public Task Test()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
