04bf158b23207ac8921592c3d99fe2fd9e894b03
刘启明 <455043818@qq.com>
2023-12-20 10:53:33 +0800

    feat: systemservice可承载多个api服务

--- .net8/Services/SystemService/aehyok.SystemService/Program.cs
+++ .net8/Services/SystemService/aehyok.SystemService/Program.cs
@@ -6,13 +6,29 @@ using aehyok.RabbitMQ;
 using aehyok.Infrastructure.Extensions;
 using aehyok.EntityFramework;
 using Microsoft.Extensions.Hosting;
+using aehyok.Swagger;
+using aehyok.Core;
+using System.Text;
+
+var serviceName = "aehyok-systemservice";
+
+var serviceTitle = "系统服务";
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 // 判断环境
+builder.Environment.EnvironmentName = "Development";
 var env = builder.Environment.IsDevelopment;
 
-builder.Host.InitHostAndConfig("aehyok.SystemService");
+builder.Host.InitHostAndConfig(serviceName);
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+builder.Services.ConfigureOptions(builder.Configuration);
+
+builder.Services.AddSwaggerGen(serviceName, serviceTitle);
 
 builder.Services.AddEFCoreAndMySql(builder.Configuration);
 
@@ -24,6 +40,13 @@ builder.Services.AddCronTask();
 
 var app = builder.Build();
 
+app.UseSwagger(serviceName, serviceTitle);
+
 app.AddRabbitMQEventBus();
+
+app.MapGet("/", () => "Hello Sunlight!");
 // 运行主机
+
+app.UseSetStartDefaultRoute();
+
 await app.RunAsync();
