60d3b07a8a4388c3279289fb9872225fd581b95b
刘启明 <455043818@qq.com>
2023-12-24 21:16:17 +0800

    chore；config 链接 update

---
+++ .net8/aehyok.Infrastructure/Filters/RequestAuthorizeFilter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aehyok.Infrastructure.Filters
+{
+    public class RequestAuthorizeFilter : IAsyncAuthorizationFilter
+    {
+        public Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
