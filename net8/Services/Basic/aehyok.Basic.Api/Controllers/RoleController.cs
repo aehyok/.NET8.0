6e9718af7bd1a3b7701ea9152ab879d6cf22c45f
刘启明 <455043818@qq.com>
2023-12-30 20:25:00 +0800

    feat; 菜单 新增 编辑 启用 禁用 删除 详情

--- net8/Services/Basic/aehyok.Basic.Api/Controllers/RoleController.cs
+++ net8/Services/Basic/aehyok.Basic.Api/Controllers/RoleController.cs
@@ -1,9 +1,11 @@
 ﻿using aehyok.Basic.Domains;
 using aehyok.Basic.Dtos;
+using aehyok.Basic.Dtos.Create;
 using aehyok.Basic.Dtos.Query;
 using aehyok.Basic.Services;
 using aehyok.Core;
 using aehyok.EntityFramework.Repository;
+using aehyok.Infrastructure.Exceptions;
 using Ardalis.Specification;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -38,6 +40,118 @@ namespace aehyok.Basic.Api.Controllers
             }
 
             return roleService.GetPagedListAsync<RoleDto>(spec, model.Page, model.Limit);
+        }
+
+        /// <summary>
+        /// 获取角色信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public Task<RoleDto> GetByIdAsync(long id)
+        {
+            return roleService.GetByIdAsync<RoleDto>(id);
+        }
+
+        /// <summary>
+        /// 添加角色
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<long> PostAsync(CreateRoleModel model)
+        {
+            var entity = this.Mapper.Map<Role>(model);
+            await roleService.InsertAsync(entity);
+            return entity.Id;
+        }
+
+        /// <summary>
+        /// 修改角色信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        public async Task<StatusCodeResult> PutAsync(long id, CreateRoleModel model)
+        {
+            var entity = await roleService.GetAsync(a => a.Id == id);
+            if (entity is null)
+            {
+                throw new ErrorCodeException(-1, "你要修改的数据不存在");
+            }
+
+            entity = this.Mapper.Map(model, entity);
+
+            await roleService.UpdateAsync(entity);
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// 启用角色
+        /// </summary>
+        /// <param name="id">角色Id</param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        [HttpPut("enable/{id}")]
+        public async Task<StatusCodeResult> EnableAsync(long id)
+        {
+            var entity = await roleService.GetAsync(a => a.Id == id);
+            if (entity is null)
+            {
+                throw new ErrorCodeException(-1, "你要启用的数据不存在");
+            }
+
+            entity.IsEnable = true;
+            await roleService.UpdateAsync(entity);
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// 禁用角色
+        /// </summary>
+        /// <param name="id">角色Id</param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        [HttpPut("disable/{id}")]
+        public async Task<StatusCodeResult> DisableAsync(long id)
+        {
+            var entity = await roleService.GetAsync(a => a.Id == id);
+            if (entity is null)
+            {
+                throw new ErrorCodeException(-1, "你要禁用的数据不存在");
+            }
+
+            entity.IsEnable = false;
+            await roleService.UpdateAsync(entity);
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// 删除角色
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        [HttpDelete("{id}")]
+        public async Task<StatusCodeResult> DeleteAsync(long id)
+        {
+            var entity = await roleService.GetAsync(a => a.Id == id);
+            if (entity is null)
+            {
+                throw new ErrorCodeException(-1, "你要删除的数据不存在");
+            }
+
+            if (entity.IsSystem)
+            {
+                throw new ErrorCodeException(-1, "禁止删除系统内置角色");
+            }
+
+            await roleService.DeleteAsync(entity);
+            return Ok();
         }
     }
 }
