c19e322a1a4897ac1e3fecdcbd026cb0cac2d0ae
刘启明 <455043818@qq.com>
2023-12-31 22:39:40 +0800

    feat: 获取角色权限

---
+++ net8/Services/Basic/aehyok.Basic/Dtos/Query/PermissionQueryModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aehyok.Basic.Dtos.Query
+{
+    public class PermissionQueryModel
+    {
+        /// <summary>
+        /// 角色编号
+        /// </summary>
+        public long RoleId { get; set; }
+    }
+}
---
+++ net8/Services/Basic/aehyok.Basic/Services/IPermissionService.cs
@@ -0,0 +1,21 @@
+﻿using aehyok.Basic.Domains;
+using aehyok.Basic.Dtos;
+using aehyok.EntityFramework.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aehyok.Basic.Services
+{
+    public interface IPermissionService: IServiceBase<Permission>
+    {
+        /// <summary>
+        /// 获取角色下拥有的菜单权限
+        /// </summary>
+        /// <param name="roleId"></param>
+        /// <returns></returns>
+        Task<List<PermissionDto>> GetMenuPermissionAsync(long roleId);
+    }
+}
