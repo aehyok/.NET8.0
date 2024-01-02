8a1af2843664d7c209089ba8b8b6349ad9451087
刘启明 <455043818@qq.com>
2024-01-02 10:43:00 +0800

    feat: 生成swagger实体备注文档

---
+++ net8/Services/Basic/aehyok.Basic/Dtos/UserRoleDto.cs
@@ -0,0 +1,37 @@
+﻿using aehyok.Infrastructure.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aehyok.Basic.Dtos
+{
+    public class UserRoleDto : DtoBase
+    {
+        /// <summary>
+        /// 角色 Id
+        /// </summary>
+        public long RoleId { get; set; }
+
+        /// <summary>
+        /// 角色名称
+        /// </summary>
+        public string RoleName { get; set; }
+
+        /// <summary>
+        /// 区域名称
+        /// </summary>
+        public string RegionName { get; set; }
+
+        /// <summary>
+        /// 区域 Id
+        /// </summary>
+        public long RegionId { get; set; }
+
+        /// <summary>
+        /// 是否默认角色
+        /// </summary>
+        public bool IsDefault { get; set; }
+    }
+}
