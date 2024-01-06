1cb6a4c719d12e49f5bc484a18a764779ac18e53
刘启明 <455043818@qq.com>
2024-01-06 22:12:41 +0800

    feat: 初始化数据和任务表修改 待执行命令

---
+++ net8/Services/SystemService/aehyok.SystemService/Migrations/20240105065032_UpdateCronTask.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aehyok.SystemService.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCronTask : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "UserRole",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                comment: "");
+
+            migrationBuilder.AddColumn<long>(
+                name: "CreatedBy",
+                table: "UserRole",
+                type: "bigint",
+                nullable: true,
+                comment: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Remark",
+                table: "UserRole",
+                type: "longtext",
+                nullable: true,
+                comment: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "UserRole",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                comment: "");
+
+            migrationBuilder.AddColumn<long>(
+                name: "UpdatedBy",
+                table: "UserRole",
+                type: "bigint",
+                nullable: true,
+                comment: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEnable",
+                table: "CronTask",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false,
+                comment: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastWriteTime",
+                table: "CronTask",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                comment: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TaskName",
+                table: "CronTask",
+                type: "longtext",
+                nullable: true,
+                comment: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRole_CreatedBy",
+                table: "UserRole",
+                column: "CreatedBy");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRole_UpdatedBy",
+                table: "UserRole",
+                column: "UpdatedBy");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRole_User_CreatedBy",
+                table: "UserRole",
+                column: "CreatedBy",
+                principalTable: "User",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRole_User_UpdatedBy",
+                table: "UserRole",
+                column: "UpdatedBy",
+                principalTable: "User",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRole_User_CreatedBy",
+                table: "UserRole");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRole_User_UpdatedBy",
+                table: "UserRole");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserRole_CreatedBy",
+                table: "UserRole");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserRole_UpdatedBy",
+                table: "UserRole");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "UserRole");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "UserRole");
+
+            migrationBuilder.DropColumn(
+                name: "Remark",
+                table: "UserRole");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "UserRole");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "UserRole");
+
+            migrationBuilder.DropColumn(
+                name: "IsEnable",
+                table: "CronTask");
+
+            migrationBuilder.DropColumn(
+                name: "LastWriteTime",
+                table: "CronTask");
+
+            migrationBuilder.DropColumn(
+                name: "TaskName",
+                table: "CronTask");
+        }
+    }
+}
---
+++ net8/aehyok.Core/HostedServices/ExecuteSeedDataService.cs
@@ -0,0 +1,139 @@
+﻿using aehyok.Core.Domains;
+using aehyok.Core.Services;
+using aehyok.Redis;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace aehyok.Core.HostedServices
+{
+    /// <summary>
+    /// 应该为所有种子数据设置一个版本号，以避免每次启动的时候执行
+    /// </summary>
+    public class ExecuteSeedDataService(IServiceScopeFactory scopeFactory,IServiceProvider services,ILogger<ExecuteSeedDataService> logger, IConfiguration configuration) : BackgroundService
+    {
+        /// <summary>
+        /// 当前配置文件的路径
+        /// </summary>
+        private string currentConfigPath = "";
+
+        /// <summary>
+        /// 当前任务是否需要执行
+        /// </summary>
+        private bool isExecute = false;
+
+        /// <summary>
+        /// 更新定时任务记录
+        /// </summary>  
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public async Task UpdateCronTask(ScheduleTask model)
+        {
+            using var scope = scopeFactory.CreateScope();
+            var cronTaskCoreService = scope.ServiceProvider.GetRequiredService<ICronTaskCoreService>();
+
+            await cronTaskCoreService.UpdateAsync(model);
+        }
+
+        /// <summary>
+        /// 获取任务是否需要执行的状态
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        private void GetExecuteStatus(ScheduleTask model)
+        {
+            if (string.IsNullOrEmpty(currentConfigPath))
+            {
+                isExecute = true;
+            }
+            var file = new FileInfo(currentConfigPath);
+
+            if (model.LastWriteTime < file.LastWriteTime)
+            {
+                isExecute = true;
+                model.LastWriteTime = file.LastWriteTime;
+            }
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            logger.LogInformation($"开始执行种子数据更新任务");
+
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            using var scope = scopeFactory.CreateScope();
+            //var redisDatabaseProvider = scope.ServiceProvider.GetService<IRedisDatabaseProvider>();
+            //var seedDataSection = configuration.GetSection("SeedData");
+
+
+            //using var scope = services.CreateScope();
+            var redisDatabaseProvider = scope.ServiceProvider.GetRequiredService<IRedisService>();
+
+            var seedInstants = scope.ServiceProvider.GetServices<ISeedData>();
+
+            var cronTaskCoreService = scope.ServiceProvider.GetRequiredService<ICronTaskCoreService>();
+
+            var list = await cronTaskCoreService.GetListAsync();
+
+            foreach (var seed in seedInstants.OrderBy(a => a.Order))
+            {
+                var taskName = seed.GetType().Name;
+                currentConfigPath = seed.ConfigPath;
+                var taskId = Guid.NewGuid().ToString();
+               
+                var model = list.FirstOrDefault(item => item.TaskName == taskName);
+
+                try
+                {
+                    //判断该任务是否启用，并且是否已同步到数据库，如果没有同步，则要写入数据库
+                    if (model is not null && !model.IsEnable)
+                    {
+                        break; //如果任务已经禁用，则不再执行
+                    }
+
+                    if(model is null)
+                    {
+                        model = await cronTaskCoreService.InsertAsync(new ScheduleTask
+                        {
+                            TaskName = taskName,
+                            IsEnable = true,
+                            
+                        });
+
+                    }
+
+                    GetExecuteStatus(model);
+
+                    if (isExecute)
+                    {
+                        logger.LogInformation($"开始执行[{taskName}]");
+
+                        if (await redisDatabaseProvider.SetAsync(taskName, taskId, TimeSpan.FromMinutes(5), CSRedis.RedisExistence.Nx))
+                        {
+                            await seed.ApplyAsync(model, UpdateCronTask);
+                        }
+
+                        logger.LogInformation($"[{taskName}]执行完成");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, ex.Message);
+                }
+                finally
+                {
+                    //await redisDatabaseProvider.LockReleaseAsync(taskName, taskId);
+                    await UpdateCronTask(model);
+                    await redisDatabaseProvider.DeleteAsync(taskName);
+                }
+            }
+
+            stopwatch.Stop();
+            logger.LogInformation($"种子数据更新任务执行完成，耗时:{stopwatch.ElapsedMilliseconds}ms");
+        }
+    }
+}
