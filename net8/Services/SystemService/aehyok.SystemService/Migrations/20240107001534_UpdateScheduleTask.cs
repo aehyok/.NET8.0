ae30d7019109d058dfccdc8996420b8435cb2fcf
刘启明 <455043818@qq.com>
2024-01-07 08:17:08 +0800

    feat:计划定时任务数据实体变更

---
+++ net8/Services/SystemService/aehyok.SystemService/Migrations/20240107001534_UpdateScheduleTask.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aehyok.SystemService.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateScheduleTask : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CronTask");
+
+            migrationBuilder.CreateTable(
+                name: "ScheduleTask",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false, comment: "")
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    LastWriteTime = table.Column<DateTime>(type: "datetime(6)", nullable: false, comment: ""),
+                    TaskName = table.Column<string>(type: "longtext", nullable: true, comment: "")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    IsEnable = table.Column<bool>(type: "tinyint(1)", nullable: false, comment: ""),
+                    IsDeleted = table.Column<bool>(type: "tinyint(1)", nullable: false, comment: "")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScheduleTask", x => x.Id);
+                },
+                comment: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ScheduleTask");
+
+            migrationBuilder.CreateTable(
+                name: "CronTask",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false, comment: "")
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    IsDeleted = table.Column<bool>(type: "tinyint(1)", nullable: false, comment: ""),
+                    IsEnable = table.Column<bool>(type: "tinyint(1)", nullable: false, comment: ""),
+                    LastWriteTime = table.Column<DateTime>(type: "datetime(6)", nullable: false, comment: ""),
+                    TaskName = table.Column<string>(type: "longtext", nullable: true, comment: "")
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CronTask", x => x.Id);
+                },
+                comment: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
