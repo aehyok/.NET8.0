0a69d79fe97eb53d0f5e5bc533ee647b7c83191c
刘启明 <455043818@qq.com>
2024-01-10 10:38:57 +0800

    fix: 暂时通过间接依赖处理引用，移除NCDP和Basic SystemService移除对Core的引用

--- net8/Libraries/aehyok.EntityFramework/DvsSaveChangeInterceptor.cs
+++ net8/Libraries/aehyok.EntityFramework/DvsSaveChangeInterceptor.cs
@@ -5,11 +5,6 @@ using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace aehyok.EntityFrameworkCore
 {
