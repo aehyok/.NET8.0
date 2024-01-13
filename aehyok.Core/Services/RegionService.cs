f9d07bd8bcb1697c2ffaaffdbf0bcf67cffe0b31
刘启明 <455043818@qq.com>
2024-01-13 09:33:01 +0800

    fix:区域初始化时要将idSequences加入

--- aehyok.Core/Services/RegionService.cs
+++ aehyok.Core/Services/RegionService.cs
@@ -1,6 +1,7 @@
 ﻿using aehyok.Core.Domains;
 using aehyok.EntityFrameworkCore.Repository;
 using aehyok.Infrastructure;
+using aehyok.Infrastructure.Exceptions;
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,5 +9,39 @@ namespace aehyok.Basic.Services
 {
     public class RegionService(DbContext dbContext, IMapper mapper) : ServiceBase<Region>(dbContext, mapper), IRegionService, IScopedDependency
     {
+        public override async Task<Region> InsertAsync(Region entity, CancellationToken cancellationToken = default)
+        {
+            if (string.IsNullOrWhiteSpace(entity.IdSequences))
+            {
+                if (entity.ParentId == 0)
+                {
+                    entity.IdSequences = $".{entity.Id}.";
+                }
+                else
+                {
+                    var parent = await this.GetAsync(a => a.Id == entity.ParentId);
+                    if (parent != null)
+                    {
+                        entity.IdSequences = $"{parent.IdSequences}{entity.Id}.";
+                        if (parent.Level == RegionLevel.自然村)
+                        {
+                            throw new ErrorCodeException(-1, "自然村不能创建下级行政区划");
+                        }
+
+                        entity.Level = parent.Level + 1;
+                    }
+                }
+            }
+
+            var exists = await this.GetAsync(a => a.Code == entity.Code);
+            if (exists != null)
+            {
+                throw new ErrorCodeException(-1, $"行政区划代码【{entity.Code}】已存在");
+            }
+
+            await base.InsertAsync(entity, cancellationToken);
+
+            return entity;
+        }
     }
 }
