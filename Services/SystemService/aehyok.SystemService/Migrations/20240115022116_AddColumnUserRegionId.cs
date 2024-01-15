87dc7d16ffab985021b4f631283c3419a0410537
刘启明 <455043818@qq.com>
2024-01-15 11:10:49 +0800

    feat: 命令行执行用户表添加区域字段

---
+++ Services/SystemService/aehyok.SystemService/Migrations/20240115022116_AddColumnUserRegionId.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aehyok.SystemService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColumnUserRegionId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "RegionId",
+                table: "User",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                comment: "区域Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_RegionId",
+                table: "User",
+                column: "RegionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_User_Region_RegionId",
+                table: "User",
+                column: "RegionId",
+                principalTable: "Region",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_User_Region_RegionId",
+                table: "User");
+
+            migrationBuilder.DropIndex(
+                name: "IX_User_RegionId",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "RegionId",
+                table: "User");
+        }
+    }
+}
