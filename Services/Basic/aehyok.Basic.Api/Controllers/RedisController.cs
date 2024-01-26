49e97d1a24ccbdfcd544c95a05984276bc5c97d7
刘启明 <455043818@qq.com>
2024-01-26 21:50:27 +0800

    feat：添加redis控制器

---
+++ Services/Basic/aehyok.Basic.Api/Controllers/RedisController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aehyok.Basic.Api.Controllers
+{
+    public class RedisController : BasicControllerBase
+    {
+        /// <summary>
+        /// 获取redis所有keys
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<dynamic> GetListAsync()
+        {
+            return null;
+        }
+    }
+}
