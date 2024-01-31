c7c30157269693ef70a1c6626b3b2a1cf9820aa9
刘启明 <455043818@qq.com>
2024-01-31 11:04:54 +0800

    chore: 移除无效的文件

--- Services/NCDP/aehyok.NCDP.Api/Program.cs
+++
@@ -1,8 +0,0 @@
-using aehyok.Core;
-
-var moduleKey = "aehyok-ncdp";
-var moduleTitle = "无代码开放平台";
-
-var builder = WebApplication.CreateBuilder(args);
-
-await builder.InitAppliation(moduleKey, moduleTitle);
