51b2c73b46f4c05055c6a8dc655834e6eed13fd2
刘启明 <455043818@qq.com>
2024-02-01 19:55:33 +0800

    feat: 添加操作日志事件 接受后无法执行待继续调试

---
+++ src/sun.Core/EventData/OperationLogEventData.cs
@@ -0,0 +1,40 @@
+﻿using sun.RabbitMQ.EventBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sun.Core.EventData
+{
+    /// <summary>
+    /// 操作日志事件传输数据
+    /// </summary>
+    public class OperationLogEventData: EventBase
+    {
+        /// <summary>
+        /// 菜单Code
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// 操作内容
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// 传递参数
+        /// </summary>
+        public string Json { get; set; }
+
+        /// <summary>
+        /// ip地址
+        /// </summary>
+        public string IpAddress { get; set; }
+
+        /// <summary>
+        /// 用户Id
+        /// </summary>
+        public long UserId { get; set; }
+    }
+}
---
+++ src/sun.Core/EventHandler/OperationLogEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using sun.Core.EventData;
+using sun.Core.Services;
+using sun.RabbitMQ.EventBus;
+using sun.Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sun.Core.EventHandler
+{
+    public class OperationLogEventHandler(IServiceScopeFactory scopeFactory): IEventHandler<OperationLogEventData>
+    {
+        public async Task HandleAsync(OperationLogEventData @event)
+        {
+            using var scope = scopeFactory.CreateScope();
+            var logger = scope.ServiceProvider.GetService<ILogger<OperationLogEventHandler>>();
+            logger.LogInformation("OperationLogEventHandler");
+            var httpContextAccessor = scope.ServiceProvider.GetService<IHttpContextAccessor>();
+            var operationLogService = scope.ServiceProvider.GetService<IOperationLogService>();
+            var ipAddress = httpContextAccessor.HttpContext.Request.GetRemoteIpAddress();
+            await operationLogService.LogAsync(@event.Code, @event.Content, @event.Json, ipAddress, @event.UserId);
+        }
+    }
+}
