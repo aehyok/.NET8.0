0a4a2af3b2379795d439138a2914764772035e36
刘启明 <455043818@qq.com>
2024-03-19 20:24:57 +0800

    feat: 定时任务和种子数据添加启用和禁用

--- src/Services/Basic/sun.Basic.Api/Controllers/ScheduleTaskController.cs
+++ src/Services/Basic/sun.Basic.Api/Controllers/ScheduleTaskController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using sun.Infrastructure.Enums;
+using sun.Basic.Services;
+using sun.Infrastructure.Exceptions;
 
 namespace sun.Basic.Api.Controllers
 {
@@ -22,6 +24,50 @@ namespace sun.Basic.Api.Controllers
         public async Task<List<ScheduleTaskDto>> GetListAsync()
         {
             return await scheduleTaskService.GetListAsync<ScheduleTaskDto>();
+        }
+
+        /// <summary>
+        /// 启用定时任务
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPut("Enable/{id}")]
+        public async Task<StatusCodeResult> ItemEnableAsync(long id)
+        {
+            var entity = await scheduleTaskService.GetByIdAsync(id);
+
+            if (entity is null)
+            {
+                throw new ErrorCodeException(-1, "您要启用的数据不存在");
+            }
+
+            entity.IsEnable = true;
+
+            await scheduleTaskService.UpdateAsync(entity);
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// 禁用定时任务
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPut("Disable/{id}")]
+        public async Task<StatusCodeResult> ItemDisableAsync(long id)
+        {
+            var entity = await scheduleTaskService.GetByIdAsync(id);
+
+            if (entity is null)
+            {
+                throw new ErrorCodeException(-1, "您要禁用的数据不存在");
+            }
+
+            entity.IsEnable = false;
+
+            await scheduleTaskService.UpdateAsync(entity);
+
+            return Ok();
         }
     }
 }
--- src/Services/Basic/sun.Basic.Api/Controllers/SeedDataController.cs
+++ src/Services/Basic/sun.Basic.Api/Controllers/SeedDataController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using X.PagedList;
 using Microsoft.AspNetCore.Authorization;
 using sun.Infrastructure.Enums;
+using sun.Core.Services;
+using sun.Infrastructure.Exceptions;
 
 namespace sun.Basic.Api.Controllers
 {
@@ -23,6 +25,50 @@ namespace sun.Basic.Api.Controllers
         public async Task<List<SeedDataTaskDto>> GetListAsync()
         {
             return await seedDataTaskService.GetListAsync<SeedDataTaskDto>();
+        }
+
+        /// <summary>
+        /// 启用种子数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPut("Enable/{id}")]
+        public async Task<StatusCodeResult> ItemEnableAsync(long id)
+        {
+            var entity = await seedDataTaskService.GetByIdAsync(id);
+
+            if (entity is null)
+            {
+                throw new ErrorCodeException(-1, "您要启用的数据不存在");
+            }
+
+            entity.IsEnable = true;
+
+            await seedDataTaskService.UpdateAsync(entity);
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// 禁用种子数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPut("Disable/{id}")]
+        public async Task<StatusCodeResult> ItemDisableAsync(long id)
+        {
+            var entity = await seedDataTaskService.GetByIdAsync(id);
+
+            if (entity is null)
+            {
+                throw new ErrorCodeException(-1, "您要禁用的数据不存在");
+            }
+
+            entity.IsEnable = false;
+
+            await seedDataTaskService.UpdateAsync(entity);
+
+            return Ok();
         }
     }
 }
