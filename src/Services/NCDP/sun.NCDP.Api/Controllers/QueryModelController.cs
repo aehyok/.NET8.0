dc45396de8369f3c95243d78289702bf06cfa0b3
刘启明 <455043818@qq.com>
2024-06-24 19:48:07 +0800

    Create QueryModelController.cs

---
+++ src/Services/NCDP/sun.NCDP.Api/Controllers/QueryModelController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sun.NCDP.Api.Controllers
+{
+
+    /// <summary>
+    /// 查询模型
+    /// </summary>
+    public class QueryModelController : NCDPControllerBase
+    {
+
+    }
+}
