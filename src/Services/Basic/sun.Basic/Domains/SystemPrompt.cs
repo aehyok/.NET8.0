af4707035838d224516eaacef61d12ab6308198b
刘启明 <455043818@qq.com>
2025-03-29 09:44:40 +0800

    feat: 添加系统提示词

---
+++ src/Services/Basic/sun.Basic/Domains/SystemPrompt.cs
@@ -0,0 +1,43 @@
+﻿using sun.EntityFrameworkCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sun.Basic.Domains
+{
+    public class SystemPrompt : AuditedEntity
+    {
+        /// <summary>
+        /// 提示词标题
+        /// </summary>
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// 提示词内容
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// 分类
+        /// </summary>
+        public SystemPromptType SystemPromptType { get; set; }
+
+        /// <summary>
+        /// 排序
+        /// </summary>
+        public int DisplayOrder { get; set; }
+
+        /// <summary>
+        /// 是否启用
+        /// </summary>
+        public bool IsEnable { get; set; }
+    }
+
+    public enum SystemPromptType
+    {
+        公众号封面图 = 1,
+        公众号卡片 = 2
+    }
+}
---
+++ src/Services/SystemService/sun.SystemService/Migrations/20250329013936_InitSystemPrompt.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sun.SystemService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitSystemPrompt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SystemPrompt",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DisplayName = table.Column<string>(type: "longtext", nullable: true, comment: "提示词标题")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Content = table.Column<string>(type: "longtext", nullable: true, comment: "提示词内容")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SystemPromptType = table.Column<int>(type: "int", nullable: false, comment: "分类"),
+                    DisplayOrder = table.Column<int>(type: "int", nullable: false, comment: "排序"),
+                    IsEnable = table.Column<bool>(type: "tinyint(1)", nullable: false, comment: "是否启用"),
+                    IsDeleted = table.Column<bool>(type: "tinyint(1)", nullable: false, comment: "是否删除"),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false, comment: "创建时间"),
+                    CreatedBy = table.Column<long>(type: "bigint", nullable: true, comment: "创建人id"),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false, comment: "修改时间"),
+                    UpdatedBy = table.Column<long>(type: "bigint", nullable: true, comment: "修改人id"),
+                    Remark = table.Column<string>(type: "longtext", nullable: true, comment: "备注")
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SystemPrompt", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SystemPrompt");
+        }
+    }
+}
